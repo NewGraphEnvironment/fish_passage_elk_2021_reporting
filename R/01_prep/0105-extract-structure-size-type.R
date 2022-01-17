a95cf26ca79becb6e121545ec75455855e854844
NewGraphEnvironment <al@newgraphenvironment.com>
2022-01-17 10:13:22 -0800

    use new pscis import function, min bridge 12m and use multiplate for streams 3.5m and deeper than 5m of fill

--- R/01_prep/0105-extract-structure-size-type.R
+++ R/01_prep/0105-extract-structure-size-type.R
@@ -3,16 +3,11 @@
 source('R/packages.R')
 source('R/functions.R')
 
-
-# pscis <- import_pscis() %>%
-#   tibble::rownames_to_column() %>%
-#   mutate(rowname = as.numeric(rowname)) %>%
-#   mutate(column_num = rowname + 4)
-
-pscis <- import_pscis(workbook_name = 'pscis_reassessments.xlsm') %>% ##was pscis_phase1.xlsm then pscis_phase2.xlsm first
-  tibble::rownames_to_column() %>%
-  mutate(rowname = as.numeric(rowname)) %>%
-  mutate(column_num = rowname + 4)
+pscis_list <- import_pscis_all()
+pscis_phase1 <- pscis_list %>% pluck('pscis_phase1')
+pscis_phase2 <- pscis_list %>% pluck('pscis_phase2')
+pscis_reassessments <- pscis_list %>% pluck('pscis_reassessments')
+pscis_all <- pscis_list %>% pluck('pscis_all')
 
 
 ##assign a value that we want to call standard fill
@@ -20,15 +15,11 @@ fill_dpth <- 3
 
 ##assign a multiplier to determine the length of a bridge above the standard
 ##that you get when you go deeper
-
-
-##standard bridge width - we go with 10m
-brdg_wdth <- 10
+##standard bridge width - we go with 12 b/c not seeing 10s in the field
+brdg_wdth <- 12
 
 ##% bigger than the channel that the bridge should be
 chn_wdth_max <- 6 ##changed this from 5
-
-
 
 ##fill depth multiplier
 ##for every 1 m deeper than 3m, we need a 1.5:1 slope so there is 3m more bridge required
@@ -36,7 +27,7 @@ fill_dpth_mult <- 3
 
 ####----------backwater candidates------------------
 ##backwatering required od<30 and slope <2, swr <1.2 see if there are options
-tab_backwater <- pscis %>%  ##changed this to pscis2!
+tab_backwater <- pscis_all %>%  ##changed this to pscis2!
   filter(barrier_result != 'Passable' &
            barrier_result != 'Unknown' &
            outlet_drop_meters < 0.3 &
@@ -44,8 +35,11 @@ tab_backwater <- pscis %>%  ##changed this to pscis2!
            culvert_slope_percent <= 2 )
 
 ##need to tweak the span so that we add 4 to the width if it is over 6m...
-str_type <- pscis %>%
-  select(column_num, pscis_crossing_id, my_crossing_reference, barrier_result, downstream_channel_width_meters, fill_depth_meters) %>%
+
+
+##this will require some ore work for cost estimates too.
+str_type <- pscis_all %>%
+  select(rowid, aggregated_crossings_id, pscis_crossing_id, my_crossing_reference, source, barrier_result, downstream_channel_width_meters, fill_depth_meters) %>%
   mutate(fill_dpth_over = fill_depth_meters - fill_dpth_mult) %>%
   mutate(crossing_fix = case_when((barrier_result == 'Barrier' | barrier_result == 'Potential')
                                 & downstream_channel_width_meters >= 2 ~ 'Replace with New Open Bottom Structure',
@@ -55,18 +49,34 @@ str_type <- pscis %>%
                                 & downstream_channel_width_meters >= 2 ~ brdg_wdth,
                                 barrier_result == 'Passable' | barrier_result == 'Unknown' ~ NA_real_,
                                 T ~ 3))  %>%
-  mutate(span_input = case_when(fill_dpth_over > 0 ~
+  mutate(span_input = case_when((barrier_result == 'Barrier' | barrier_result == 'Potential') & fill_dpth_over > 0 & !crossing_fix %ilike% 'Simulation' ~
                                   (brdg_wdth + fill_dpth_mult * fill_dpth_over),  ##1m more fill = 3 m more bridge
                                 T ~ span_input)) %>%
   mutate(span_input = case_when(span_input < (downstream_channel_width_meters + 4) & ##span not need be extended if already 4m bigger than channel width
                                   downstream_channel_width_meters > chn_wdth_max ~
                                   (downstream_channel_width_meters - chn_wdth_max) + span_input,  ##for every m bigger than a 5 m channel add that much to each side in terms of span
                                 T ~ span_input)) %>%
-  mutate(span_input = round(span_input, 1)) %>%
-  arrange(column_num)
+  ##let's add an option that if the stream is under 3.5m wide and under more than 5m of fill we do a streambed simulation with a 4.5m embedded multiplate like 4607464 on Flathead fsr
+  mutate(crossing_fix = case_when((barrier_result == 'Barrier' | barrier_result == 'Potential')
+                                  & downstream_channel_width_meters > 2 &
+                                    downstream_channel_width_meters <= 3.5 &
+                                    fill_depth_meters > 5 ~ 'Replace Structure with Streambed Simulation CBS',
+                                  T ~ crossing_fix),
+         span_input = case_when((barrier_result == 'Barrier' | barrier_result == 'Potential')
+                                  & downstream_channel_width_meters > 2 &
+                                  downstream_channel_width_meters <= 3.5 &
+                                  fill_depth_meters > 5 ~ 4.5,
+                                  T ~ span_input)) %>%
+  mutate(span_input = round(span_input, 1))
 
-##burn to a csv so you can copy and paste into spreadsheet
+##burn to a csvs so we can copy and paste into spreadsheet (could make a function to do this all at once....)
+str_type %>%
+  filter(source %ilike% 'phase1') %>%
+  readr::write_csv(file = paste0(getwd(), '/data/inputs_extracted/pscis1_str_type.csv'))
+str_type %>%
+  filter(source %ilike% 'phase2') %>%
+  readr::write_csv(file = paste0(getwd(), '/data/inputs_extracted/pscis2_str_type.csv'))
+str_type %>%
+  filter(source %ilike% 'reasses') %>%
+  readr::write_csv(file = paste0(getwd(), '/data/inputs_extracted/pscis_reassessments_str_type.csv'))
 
-# str_type %>% readr::write_csv(file = paste0(getwd(), '/data/extracted_inputs/pscis1_str_type.csv'))
-# str_type %>% readr::write_csv(file = paste0(getwd(), '/data/extracted_inputs/pscis2_str_type.csv'))
-str_type %>% readr::write_csv(file = paste0(getwd(), '/data/extracted_inputs/pscis_reassessments_str_type.csv'))

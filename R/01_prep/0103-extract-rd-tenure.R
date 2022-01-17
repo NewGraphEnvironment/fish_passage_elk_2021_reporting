0e0e2b6c7dcd3742b1b64966c92c61e4ddf7772b
NewGraphEnvironment <al@newgraphenvironment.com>
2022-01-17 09:06:53 -0800

    just clean up rows

--- R/01_prep/0103-extract-rd-tenure.R
+++ R/01_prep/0103-extract-rd-tenure.R
@@ -14,11 +14,9 @@ source('R/private_info.R')
 
 
 pscis_list <- import_pscis_all()
-
 pscis_phase1 <- pscis_list %>% pluck('pscis_phase1')
 pscis_phase2 <- pscis_list %>% pluck('pscis_phase2')
 pscis_reassessments <- pscis_list %>% pluck('pscis_reassessments')
-
 pscis_all <- pscis_list %>% pluck('pscis')
 
 pscis_all_sf <- pscis_all %>%

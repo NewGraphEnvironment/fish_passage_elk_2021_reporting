350e0355f2be5daa410c15b1786ce171d4d8dc35
NewGraphEnvironment <al@newgraphenvironment.com>
2022-01-19 06:35:03 -0800

    use functions for backup and folder creation.  add special cases

--- R/01_prep/0115-photos-backup-resize.R
+++ R/01_prep/0115-photos-backup-resize.R
@@ -5,171 +5,78 @@
 ##make a composite image of all the most relevant culvert shots for easy display in leaflet yo
 ##get rid of the AAE files from the iphone.
 
+source('R/functions.R')
+source('R/packages.R')
+source('R/private_info.R')
 
 
-library(tidyverse)
-library(magick)
+pscis_list <- import_pscis_all()
+pscis_phase1 <- pscis_list %>% pluck('pscis_phase1')
+pscis_phase2 <- pscis_list %>% pluck('pscis_phase2')
+pscis_reassessments <- pscis_list %>% pluck('pscis_reassessments')
+pscis_all <- pscis_list %>% pluck('pscis_all')
 
 ##back up your photos onto the D drive.
 
-##create a folder to copy the photos to
-
-
-##get teh name of the folder we are in
-bname <- basename(dirname(dirname(getwd())))
-
-##input the name of the file we want to copy.  We should get a list of files next time and then purrr::map the procedure
-filename = "al"
-
-##here we back everything up to the D drive
-targetdir = paste0("D:/New_Graph/backups/photos/", bname, "/")
-dir.create(targetdir)
-
-targetdir = paste0("D:/New_Graph/backups/photos/", bname, "/", filename)
-dir.create(targetdir)
-
-
-##path to the photos
-path <- paste0("C:/Users/allan/OneDrive/New_Graph/Current/", bname, '/data/photos/', filename)
-
-filestocopy <- list.files(path = path,
-                          full.names = T)
-
-#copy over the photos in the al folder -- this is done already
-file.copy(from=filestocopy, to=targetdir,
-          overwrite = F, recursive = FALSE,
-          copy.mode = TRUE)
-
-
-
-#----------backup kyle---------------------------------------------
-##input the name of the file we want to copy.  We should get a list of files next time and then purrr::map the procedure
-filename = "kyle"
-
-##here we back everything up to the D drive
-targetdir = paste0("D:/New_Graph/backups/photos/", bname, "/")
-dir.create(targetdir)
-
-targetdir = paste0("D:/New_Graph/backups/photos/", bname, "/", filename)
-dir.create(targetdir)
-
-##path to the photos
-path <- paste0("C:/Users/allan/OneDrive/New_Graph/Current/", bname, '/data/photos/', filename)
-
-filestocopy <- list.files(path = path,
-                          full.names = T)
-
-
-file.copy(from=filestocopy, to=targetdir,
-          overwrite = F, recursive = FALSE,
-          copy.mode = TRUE)
-
-##this scales everything and converts everything to jpg - we had problems with sizes being slightly too large last year
-##so we will do manually this year with powertools type thing (image resizer for windows)
-# img_resize_convert <- function(img){
-#   image <- image_read(img)
-#   image_scaled <- image_scale(image,"1440x1080!")
-# image_write(image_scaled, path = paste0(path, '/', tools::file_path_sans_ext(basename(img)), '.JPG'), format = 'jpg')
-# }
-
-#---------------------------------------backup dallas----------------------------------------------------
-##input the name of the file we want to copy.  We should get a list of files next time and then purrr::map the procedure
-filename = "dallas"
-
-##here we back everything up to the D drive
-targetdir = paste0("D:/New_Graph/backups/photos/", bname, "/")
-dir.create(targetdir)
-
-targetdir = paste0("D:/New_Graph/backups/photos/", bname, "/", filename)
-dir.create(targetdir)
-
-##path to the photos
-path <- paste0("C:/Users/allan/OneDrive/New_Graph/Current/", bname, '/data/photos/', filename)
-
-filestocopy <- list.files(path = path,
-                          full.names = T)
-
-
-file.copy(from=filestocopy, to=targetdir,
-          overwrite = F, recursive = FALSE,
-          copy.mode = TRUE)
-
-
+fpr_photos_backup(filename = 'al')
+fpr_photos_backup(filename = 'tammy')
+fpr_photos_backup(filename = 'brody')
+fpr_photos_backup(filename = 'kyle')
+fpr_photos_backup(filename = 'nupqu')  ##this doesn't work becasue the photos are already in folders - done by hand
 
 
 #---------------------------convert al------------------------------------------------------------
-## we  want to convert our png to jpeg in case we want them for something
-img_resize_convert <- function(img){
-  image <- image_read(img)
-  image_scaled <- image_scale(image,"1440x1080!")
-  image_write(image_scaled, path = paste0(path, '/', tools::file_path_sans_ext(basename(img)), '.JPG'), format = 'jpg')
-}
 
-##input the name of the file we want to copy.  We should get a list of files next time and then purrr::map the procedure
-filename = "al"
-##path to the photos
-path <- paste0("C:/Users/allan/OneDrive/New_Graph/Current/", bname, '/data/photos/', filename)
-filestocopy <- list.files(path = path,
-                          full.names = T)
-filestoconvert <- grep('.PNG', filestocopy, value=TRUE)
-filestoconvert %>%
-  purrr::map(img_resize_convert)
-############ remove the png files that are now converted to jpg
-##identify all the png files in the folder
-filesremove <- grep('.PNG', filestocopy, value=TRUE)
-file.remove(filesremove)
-
-#####---------------------------------convert kyle-------------------------------------------------------------------
-##input the name of the file we want to copy.  We should get a list of files next time and then purrr::map the procedure
-filename = "kyle"
-##path to the photos
-path <- paste0("C:/Users/allan/OneDrive/New_Graph/Current/", bname, '/data/photos/', filename)
-filestocopy <- list.files(path = path,
-                          full.names = T)
-filestoconvert <- grep('.PNG', filestocopy, value=TRUE)
-filestoconvert %>%
-  purrr::map(img_resize_convert)
-############ remove the png files that are now converted to jpg
-##identify all the png files in the folder
-filesremove <- grep('.PNG', filestocopy, value=TRUE)
-file.remove(filesremove)
+#
+# ##input the name of the file we want to copy.  We should get a list of files next time and then purrr::map the procedure
+# filename = "al"
+# ##path to the photos
+# path <- paste0("C:/Users/allan/OneDrive/New_Graph/Current/", bname, '/data/photos/', filename)
+# filestocopy <- list.files(path = path,
+#                           full.names = T)
+# filestoconvert <- grep('.PNG', filestocopy, value=TRUE)
+# filestoconvert %>%
+#   purrr::map(img_resize_convert)
+# ############ remove the png files that are now converted to jpg
+# ##identify all the png files in the folder
+# filesremove <- grep('.PNG', filestocopy, value=TRUE)
+# file.remove(filesremove)
+#
+# #####---------------------------------convert kyle-------------------------------------------------------------------
+# ##input the name of the file we want to copy.  We should get a list of files next time and then purrr::map the procedure
+# filename = "kyle"
+# ##path to the photos
+# path <- paste0("C:/Users/allan/OneDrive/New_Graph/Current/", bname, '/data/photos/', filename)
+# filestocopy <- list.files(path = path,
+#                           full.names = T)
+# filestoconvert <- grep('.PNG', filestocopy, value=TRUE)
+# filestoconvert %>%
+#   purrr::map(img_resize_convert)
+# ############ remove the png files that are now converted to jpg
+# ##identify all the png files in the folder
+# filesremove <- grep('.PNG', filestocopy, value=TRUE)
+# file.remove(filesremove)
 
 
 
 ###make the folders that we will drag our photos into
-##get the names of your pscis files
-workbooks <-  list.files(path = 'data', pattern = "pscis", all.files = F) %>%
-  grep(pattern = '~', invert = T, value = T)
-
-
-source('R/functions.R')
-
-df <- workbooks %>%
-  map_df(import_pscis)
-
-##create the data folder
-dir.create(paste0(getwd(), '/data'))
 
 ##create the photos folder
 dir.create(paste0(getwd(), '/data/photos'))
 
-folderstocreate <- df %>%
+##first the my_crossing_reference_folders
+folderstocreate <- pscis_all %>%
   filter(!is.na(my_crossing_reference)) %>%
   distinct(my_crossing_reference) %>%
   pull(my_crossing_reference) %>%
   as.character()
 
 
-##function that builds the folders
-make_photo_folders <- function(xing){
-  dir.create(paste0(getwd(), '/data/photos/', xing))
-}
-
 folderstocreate %>%
-  purrr::map(make_photo_folders)
+  purrr::map(fpr_make_photo_folders)
 
 ##do the same for our pscis crossings
-folderstocreate <- df %>%
+folderstocreate <- pscis_all %>%
   filter(!is.na(pscis_crossing_id)) %>%
   distinct(pscis_crossing_id) %>%
   pull(pscis_crossing_id) %>%
@@ -177,8 +84,13 @@ folderstocreate <- df %>%
 
 
 folderstocreate %>%
-  purrr::map(make_photo_folders)
+  purrr::map(fpr_make_photo_folders)
 
+##we have special cases that do not have excel inputs
 
+folders_special_cases <- c(197542, 4605826)
+
+folders_special_cases %>%
+  purrr::map(fpr_make_photo_folders)
 
 

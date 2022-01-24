978ea7382a1ea635ef76934e691243cb6c985db1
NewGraphEnvironment <al@newgraphenvironment.com>
2022-01-24 10:54:31 -0800

    delete junk and tweak QA function for detailed review

--- R/01_prep/0120-photos-amalgamate-culvert.R
+++ R/01_prep/0120-photos-amalgamate-culvert.R
@@ -1,4 +1,5 @@
-##we want a composite photo for viewing that has the 6 key photos of each crossing
+# This file is used to confirm that we have one of each of the photos required for PSCIS upload and to
+# build a composite photo for viewing that has the 6 key photos of each crossing
 source('R/packages.R')
 source('R/functions.R')
 
@@ -7,73 +8,42 @@ source('R/functions.R')
 site_id_list <- list.files(path = paste0(getwd(), '/data/photos/'), full.names = T) %>%
   basename()
 
-##lets look to see that we have all the right photo names - the sorting has nothing to do with it
-##but might be useful for QA if we add the total and check that it is equiv - leave for now...
-find_photo_names <- function(site_id){
-  list.files(path = paste0(getwd(), '/data/photos/', site_id), full.names = T) %>%
-    stringr::str_subset(., 'barrel|outlet|upstream|downstream|road|inlet') %>%
-    as_tibble() %>%
-    mutate(sort = case_when(
-      value %ilike% 'road' ~ 1,
-      value %ilike% 'inlet' ~ 2,
-      value %ilike% 'upstream' ~ 3,
-      value %ilike% 'barrel' ~ 4,
-      value %ilike% 'outlet' ~ 5,
-      value %ilike% 'downstream' ~ 6,
-    )) %>%
-    arrange(sort) %>%
-    select(-sort)
-}
 
 
-##if you look at this "by hand" you can see tibbles with less than 6 rows so those should be fixed.
 ##we had a couple of extra litte files in the folder that were a problem. make sure the file is only photo folders
-##we should be able to filter the list so that we get a quick read on which ones are short photos but we will need to do that another time.
-##this does not identify when there are duplicates and one photo is missing!! need to revise
+
 photo_names <- site_id_list %>%
-  map(find_photo_names)
+  map(fpr_photo_qa) %>%
+  purrr::set_names(site_id_list) %>%
+  bind_rows(.id = 'site_id') %>%
+  pivot_wider(names_from = x,
+              values_from = x)
+
+
+##if the above df build throws a list column warning then there are duplicates.  Deal with those first (should build a filter next time)
+## and then run the filter below to find the sites with missing photos
+colnms <- names(photo_names)
+
+photo_missing <- photo_names %>%
+  filter(across(all_of(colnms), is.na))  ##filter_at(vars(all_of(colnms)), any_vars(is.na(.)))
+
+
+# So - fpr_photo_qa will only give us site_ids when at least 1 of the photos we search for are there!
+# we are missing folders here because we have some dam sites that do not have
+#  for this reason we pull our list of files to do the photo amalgamation on from photo_names dataframe above
 
 
 ##once you review the test results and make sure they are all there you are good then run your function over the list
-site_id_list %>%
-  map(make_photo_comp_cv)
+## test on a small sample
+# test <- photo_names$site_id %>% head()
 
 
+test %>%
+  map(fpr_photo_amalg_cv)
 
-
-#could try to get the names of the photos printed on the photos but might not be worth it...
-
-##------------used this to make the function
-# site_id <- '3054'
-# # photos_id1 <- list.files(path = paste0(getwd(), '/data/photos/', site_id), full.names = T) %>%
-# #   stringr::str_subset(., 'barrel|outlet|upstream')
-# # photos_id2 <- list.files(path = paste0(getwd(), '/data/photos/', site_id), full.names = T) %>%
-# #   stringr::str_subset(., 'downstream|road|inlet')
-#
-# ##read in the photos
-# # photos_images1 <- image_read(photos_id1)
-#
-# photos_images1 <- list.files(path = paste0(getwd(), '/data/photos/', site_id), full.names = T) %>%
-#   stringr::str_subset(., 'barrel|outlet|upstream') %>%
-#   image_read()
-#
-# photos_images2 <- list.files(path = paste0(getwd(), '/data/photos/', site_id), full.names = T) %>%
-#   stringr::str_subset(., 'downstream|road|inlet') %>%
-#   image_read()
-# #
-# photos_stack1 <-image_append(image_scale(photos_images1, "x420")) ##1/3 the width 373.33 and half the original height
-# photos_stack2 <- image_append(image_scale(photos_images2, "x420"))
-# # photos_stack1 <-image_append(image_scale(photos_images1))
-# # photos_stack2 <- image_append(image_scale(photos_images2))
-#
-# photos_stack <- c(photos_stack1, photos_stack2)
-#
-# # photos_stacked <- image_append(image_scale(photos_stack, "x200"), stack = T)
-# photos_stacked <- image_append(image_scale(photos_stack), stack = T)
-# photos_stacked
-#
-# image_write(photos_stacked, path = paste0(getwd(), '/data/photos/', site_id, '/crossing_all.JPG'), format = 'jpg')
-
+## make the amalgamated photos for each of the crossings
+photo_names$site_id %>%
+  map(fpr_photo_amalg_cv)
 
 
 ########################---------------resize our generic image

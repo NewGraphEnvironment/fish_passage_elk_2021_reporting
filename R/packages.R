c5e09820e061602eeebe6606766ce826fe47bd92
NewGraphEnvironment <al@newgraphenvironment.com>
2022-02-09 16:12:43 -0800

    move plotKML to the top b/c glitching out

--- R/packages.R
+++ R/packages.R
@@ -1,6 +1,8 @@
 # install.packages('pacman')
 
+
 pacman::p_load(
+  plotKML,  #plot kml needed to go ahead of other packages for some reason.... not sure why
   tidyverse,
   readwritesqlite,
   sf,
@@ -8,7 +10,6 @@ pacman::p_load(
   janitor,
   leafem,
   leaflet,
-  plotKML,
   kableExtra,
   httr,
   RPostgres,
@@ -24,6 +25,7 @@ pacman::p_load(
   forcats,
   bookdown,
   fasstr,
+  tidyhydat,
   # flextable,
   english,
   leaflet.extras,
@@ -34,6 +36,8 @@ pacman::p_load(
   # gert  ##to track git moves
   ##leafpop I think
   )
+
+#
 
 
 

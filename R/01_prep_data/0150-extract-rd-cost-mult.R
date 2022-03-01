56494dc4373a3ea7dbcbf083931ddce3ae544e5a
NewGraphEnvironment <al@newgraphenvironment.com>
2022-03-01 12:26:33 -0800

    update to have min of 20k/m and multiplier for moti roads to max 15 (30) times vs 20 (40)

--- R/01_prep_data/0150-extract-rd-cost-mult.R
+++ R/01_prep_data/0150-extract-rd-cost-mult.R
@@ -115,17 +115,17 @@ tab_cost_rd_mult <- pscis_rd %>%
   # mutate(road_surface_mult = NA_real_, road_class_mult = NA_real_) %>%
   mutate(road_class_mult = case_when(my_road_class == 'local' ~ 4,
                                      my_road_class == 'collector' ~ 4,
-                                     my_road_class == 'arterial' ~ 20,
-                                     my_road_class == 'highway' ~ 20,
-                                     my_road_class == 'rail' ~ 20,
+                                     my_road_class == 'arterial' ~ 15,
+                                     my_road_class == 'highway' ~ 15,
+                                     my_road_class == 'rail' ~ 15,
                                      T ~ 1))  %>%
   mutate(road_surface_mult = case_when(my_road_surface == 'loose' |
                                          my_road_surface == 'rough' ~
                                          1,
                                        T ~ 2)) %>%
   # mutate(road_type_mult = road_class_mult * road_surface_mult) %>%
-  mutate(cost_m_1000s_bridge = road_surface_mult * road_class_mult * 17.5,  #changed from 12.5 due to inflation
-         cost_embed_cv = road_surface_mult * road_class_mult * 25) %>%
+  mutate(cost_m_1000s_bridge = road_surface_mult * road_class_mult * 20,  #changed from 12.5 due to inflation
+         cost_embed_cv = road_surface_mult * road_class_mult * 40) %>%
   # mutate(cost_1000s_for_10m_bridge = 10 * cost_m_1000s_bridge) %>%
   distinct( .keep_all = T) %>%
   tidyr::drop_na() %>%

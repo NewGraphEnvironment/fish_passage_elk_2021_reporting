c036e23dc0c806aede01977ce4ecace20a229748
NewGraphEnvironment <al@newgraphenvironment.com>
2022-03-05 12:50:42 -0800

    add wshd stats packages. hash out unused

--- R/packages.R
+++ R/packages.R
@@ -2,7 +2,8 @@
 
 
 pacman::p_load(
-  plotKML,  #plot kml needed to go ahead of other packages for some reason.... not sure why
+  # plotKML,  #plot kml needed to go ahead of other packages for some reason and wants to reinstall everytime.... not sure why. hash out for now
+  raster, #load this dog before dplyr yo
   tidyverse,
   readwritesqlite,
   sf,
@@ -26,19 +27,23 @@ pacman::p_load(
   bookdown,
   fasstr,
   tidyhydat,
+
+  elevatr,
+  rayshader,
   # flextable,
   english,
   leaflet.extras,
-  ggdark,
-  pdftools,
-  geojsonsf,
-  bit64 ##to make integer column type for pg
+  ggdark
+  # pdftools,
+  # rgl,
+  # geojsonsf,
+  # bit64 ##to make integer column type for pg
   # gert  ##to track git moves
   ##leafpop I think
   )
 
-#
-
+# we need the development version of pagedown as of 20200303 https://github.com/rstudio/pagedown/issues/265
+# remotes::install_github('rstudio/pagedown')
 
 
 pacman::p_load_gh("poissonconsulting/fwapgr",

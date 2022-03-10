dbf4325f5ae820f0185687733b2b3cd53958f9a1
NewGraphEnvironment <al@newgraphenvironment.com>
2022-03-09 16:02:30 -0800

    initial commit

---
+++ R/02_prep_reporting/0190-build-html-map-tables.R
@@ -0,0 +1,18 @@
+# lets just make the html tables when we need to
+
+
+# going to turn it off for a minute
+##for now we just print out the phase 2 tables.
+# There are issues with the phase 1s even now we generalized the input column so we can use modelled_crossing_id
+pscis_all %>%
+  # distinct(pscis_crossing_id) %>%
+  filter(source %ilike% 'phase2') %>%
+  pull(pscis_crossing_id) %>%
+  map(fpr_print_tab_bcfp_html)
+
+# build all the cv tables for interactive map
+pscis_all %>%
+  distinct(pscis_crossing_id) %>%
+  # filter(source %ilike% 'phase2') %>%
+  pull(pscis_crossing_id) %>%
+  map(fpr_print_tab_cv_html)

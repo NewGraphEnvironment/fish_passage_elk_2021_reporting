8c13423594ff3125bb2d8a6224cd318e421220ed
NewGraphEnvironment <al@newgraphenvironment.com>
2022-03-11 07:29:56 -0800

    remove rotated photos since they revert in leaflet

--- R/02_prep_reporting/0180-photos-extract-metadata.R
+++ R/02_prep_reporting/0180-photos-extract-metadata.R
@@ -8,15 +8,17 @@ photo_metadata <- exifr::read_exif('data/photos',recursive=T)  %>%
   mutate(url  = paste0('https://github.com/NewGraphEnvironment/fish_passage_elk_2021_reporting/raw/master/',
                        source_file)) %>%
          # base = tools::file_path_sans_ext(filename)) %>%
-  filter(file_name %like% '_k_')
+  filter(file_name %like% '_k_' &
+           !file_name %like% 'rotated') # here is a hack so that we don't get doubles of the photos from the google flip phone
+
+# 'https://newgraphenvironment.github.io/fish_passage_elk_2021_reporting/fish_passage_elk_2021_reporting/'
 
 
 conn <- rws_connect("data/bcfishpass.sqlite")
 rws_list_tables(conn)
-# rws_drop_table("photo_metadata", conn = conn) ##now drop the table so you can replace it
+rws_drop_table("photo_metadata", conn = conn) ##now drop the table so you can replace it
 rws_write(photo_metadata, exists = F, delete = TRUE,
           conn = conn, x_name = "photo_metadata")
 rws_list_tables(conn)
 rws_disconnect(conn)
-
 
